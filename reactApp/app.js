e3e7b941dfdbff8fa4260f201952181563b8dd6c
borajimin <borajimin@gmail.com>
2017-10-06 14:45:22 -0700

    message

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(React.createElement('p',null,'Hi'),
-   document.getElementById('root'));
+
+class Todo extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <li><task type='text'>X</task>{this.props.data}</li>
+    )
+  }
+}
+
+class TodoList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  componentWillMount(){
+    var dummyData = ['what','why','when','how','which'];
+  }
+  render(){
+    return(
+      <ul>
+        {dummyData.map((data)=>(<Todo data={data}/>))}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
