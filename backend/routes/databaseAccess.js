edba59ab20652c743d500d1b4a04f5475309de84
borajimin <borajimin@gmail.com>
2018-07-11 15:38:18 -0700

    backed modification added

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const todo = require('../models/TodoItem.js');
 
 router.get('/add', (req, res) => {
   res.send('Hello');
 });
 
-module.exports = router;   
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
+const mongoose = require('mongoose');
 
+
+mongoose.connect(MONGODB_URI);
+
+const testTodo = new TodoItem({
+  task: "test task"
+});
+
+testTodo.save()
+  .then(response => {
+    res.send(response);
+  })
+  .catch(error => {
+    res.send(error);
+  })
+});
 
 app.use('/db', dbRoutes);
 // This line makes the build folder publicly available.
